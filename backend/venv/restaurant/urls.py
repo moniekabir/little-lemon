7cabd7ec8f1c5166ea78cb3393ff7476687733e0
Monie Kabir <moniekabir@gmail.com>
2023-07-03 18:42:15 +0330

    Add booking API endpoints and serializers

--- backend/venv/restaurant/urls.py
+++ backend/venv/restaurant/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from restaurant import views
+
+app_name = 'restaurant'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu'),
+    # path('menu/items/', views.MenuItemView.as_view(), name='menu_items'),
 ]
