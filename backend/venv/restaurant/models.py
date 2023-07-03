e97011aae71f8eeba0dc1a6d49890742e3a49b85
Monie Kabir <moniekabir@gmail.com>
2023-07-03 13:41:33 +0330

    Add Menu and Booking models, register them in admin, and apply necessary migrations

--- backend/venv/restaurant/models.py
+++ backend/venv/restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Name')
+    no_of_guests = models.IntegerField(null=True, default=0, verbose_name='Number of Guests')
+    booking_date = models.DateTimeField(verbose_name='Booking Date')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Bookings'
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price')
+    inventory = models.IntegerField(default=0, verbose_name='Inventory')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Menus'
